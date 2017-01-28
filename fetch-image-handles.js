6f22d877e9fee7074588437003dd70ad46fb6a9e
Sampo Osmonen <sampo@meiko.fi>
2017-01-28 13:30:51 +0200

    fix image fetching

--- fetch-image-handles.js
+++ fetch-image-handles.js
@@ -15,6 +15,8 @@ const Image     = require('./api.js').Image;
 var total = 1;
 
 function getImagesFromIndex(era, from) {
+    if (era > 3) return;
+
     const baseUrl = `http://sa-kuva.fi/neo?tem=webneo_dynlist_fin_saint&startdate=19000101&enddate=20000101&xsearch_content=&withoutdate=1&view_name=SA_archiveX&publication=${era}&verification=7aa7d22810600c57792a12b661bdefc8&from=`;
     const url = baseUrl + from;
 
@@ -35,23 +37,32 @@ function getImagesFromIndex(era, from) {
                 images.push(image);
             });
 
-            images.forEach(i => {
+            Promise.all(images.map(i => {
                 const imageProps = {sa_id: i.sa_id, era: i.era};
-                Image.count({sa_id: i.sa_id}, (err, count) => {
-                    if (count > 0) {
-                        return null;
-                    }
-                    return new Image(imageProps)
-                        .save();
+                return new Promise((resolve, reject) => {
+                    Image.count({sa_id: i.sa_id}, (err, count) => {
+                        if (count > 0) {
+                            return resolve();
+                        }
+                        return new Image(imageProps)
+                            .save()
+                            .then(_ => resolve())
+                            .catch(_ => console.log);
+                    });
                 });
-            });
-
-            total += images.length;
-            console.log(`${total} found`);
-            if (images.length > 0) getImagesFromIndex(era, total);
-            else {
-                console.log(html);
-            }
+            }))
+            .then(_ => {
+                total += images.length;
+                console.log(`${total} found`);
+                if (images.length > 0) {
+                    setTimeout(() => getImagesFromIndex(era, total), 30);
+                }
+                else {
+                    console.log(html);
+                    getImagesFromIndex(era + 1, 1);
+                }
+            })
+            .catch(console.log);
         }
         else {
             console.log(error);
@@ -60,5 +71,3 @@ function getImagesFromIndex(era, from) {
 }
 
 getImagesFromIndex(1, 1);
-getImagesFromIndex(2, 1);
-getImagesFromIndex(3, 1);
